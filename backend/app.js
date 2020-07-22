d689ab4fc00fc388338dbd388909389cbbb862b5
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-07-22 10:42:47 -0400

    Changes for Register and login

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const db = require('./db');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const usersRegister = require('./routes/register');
+const usersLogin = require('./routes/login');
 
 const app = express();
 
@@ -22,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter(db));
 app.use('/users', usersRouter(db));
+app.use('/register', usersRegister(db));
+app.use('/login', usersLogin(db));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
