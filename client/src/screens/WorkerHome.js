11751ac2f46d503f7ed2bd7f60b76631c56b5af6
fsajadi <s.sfatemeh@gmail.com>
2020-07-24 12:14:33 -0400

    ad worker home page info

--- client/src/screens/WorkerHome.js
+++ client/src/screens/WorkerHome.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './WorkerHome.css';
 import ProfilePicture from '../images/profilePicture.PNG';
+import { AuthContext } from '../components/AuthProvider';
 
 function WorkerHome() {
+  const auth = useContext(AuthContext);
   return (
-    <img className='avatar' src={ProfilePicture}></img>
+    <>
+      <img className='avatar' src={ProfilePicture}></img>
+      <div className='worker-info'>
+        <p className='worker-home'>{auth.user.firstname}</p>
+        <p className='worker-home'>{auth.user.lastname}</p>
+        <p className='worker-home'>{auth.user.email}</p>
+        <p className='worker-home'>{auth.user.address}</p>
+        <p className='worker-home'>{auth.user.phoneNumber}</p>
+        <p className='worker-home'>{auth.user.type}</p>
+      </div>
+    </>
   );
 }
 
