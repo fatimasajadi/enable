837062b07a4c89aa95c2e61ae29102dc1fb7a41d
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-07-25 23:26:28 -0400

    Worker Pending Request tab changes

--- backend/db/schema/create.sql
+++ backend/db/schema/create.sql
@@ -10,3 +10,14 @@ CREATE TABLE users (
   type VARCHAR(50) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE contracts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  worker_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  patient_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  service_date DATE,
+  description VARCHAR(255),
+  rate INTEGER,
+  type_of_pay VARCHAR(255),
+  status TEXT
+);
