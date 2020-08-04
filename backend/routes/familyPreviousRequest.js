bb4d7c736c737944a455c6a63cb261e6181be721
fsajadi <s.sfatemeh@gmail.com>
2020-08-04 13:08:04 -0400

    get family requests from db & update the value state

--- backend/routes/familyPreviousRequest.js
+++ backend/routes/familyPreviousRequest.js
@@ -6,18 +6,18 @@ module.exports = db => {
 
   //To get the details of Previous work
   router.get('/', (req, res) => {
-    let userId = req.body.family_id;
-    if(req.session['user_id'] === userId){
-      const query = {
-        text: 'SELECT * FROM contracts where from_date < CURRENT_TIMESTAMP and to_date < CURRENT_TIMESTAMP and patient_id = $1;',
-        values:[userId]
-      };
-        db.query(query)
-          .then(result => res.json(result))
-          .catch(err => console.log(err));
-      }else{
-        res.status(401);
-        res.json({ error: "Incorrect user" })
-      }
+
+    let userId = req.session['user_id'];
+    const query = {
+      text: 'SELECT * FROM contracts where from_date < CURRENT_TIMESTAMP and to_date < CURRENT_TIMESTAMP and patient_id = $1;',
+      values: [userId]
+    };
+    db.query(query)
+      .then(result => res.json(result))
+      .catch(err => {
+        console.error(err);
+        res.status(500).send(err)
+      });
   });
+  return router;
 }
