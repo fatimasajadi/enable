9f66eb5732361a9029caeff274a704e3e0f43aaa
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-08-04 14:57:28 -0400

    Changes for updating status in DB

--- backend/routes/workerPendingRequest.js
+++ backend/routes/workerPendingRequest.js
@@ -27,54 +27,6 @@ module.exports = db => {
         console.error(err);
         res.status(500).send(err)
       });
-
-
-
-  });
-
-
-  //When user clicks accept button
-
-  router.post('/', (req, res) => {
-    console.log("Started")
-    let userId = req.body.worker_id;
-    console.log(userId)
-
-    if (req.session['user_id'] === userId) {
-      const query = {
-        text: 'UPDATE contracts set status = $1 where worker_id = $2;',
-        values: ["ACCEPTED", userId]
-      };
-      db.query(query)
-        .then({ status: "ACCEPTED" })
-        .catch(err => console.log(err));
-
-    } else {
-      res.status(401);
-      res.json({ error: "Incorrect user" })
-    }
-  });
-
-
-  //When user clicks reject button
-
-  router.post('/', (req, res) => {
-    console.log("Started")
-    let userId = req.body.worker_id;
-
-    if (req.session['user_id'] === userId) {
-      const query = {
-        text: 'UPDATE contracts set status = $1 where worker_id = $2;',
-        values: ["REJECTED", userId]
-      };
-      db.query(query)
-        .then({ status: "REJECTED" })
-        .catch(err => console.log(err));
-
-    } else {
-      res.status(401);
-      res.json({ error: "Incorrect user" })
-    }
   });
 
   return router;
