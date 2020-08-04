c1547db26c968e306be431821a99bcd2e7420d94
fsajadi <s.sfatemeh@gmail.com>
2020-08-03 22:30:53 -0400

    leftjoin contracts & users tables to get the patient firstname & lastname from db

--- backend/routes/workerPendingRequest.js
+++ backend/routes/workerPendingRequest.js
@@ -9,12 +9,20 @@ module.exports = db => {
     let userId = req.session['user_id'];
     const query = {
 
-      text: 'SELECT * FROM contracts where status = $1 and worker_id = $2;',
+      text: 'SELECT contracts.*, users.firstName, users.lastName FROM contracts LEFT JOIN users ON users.id = contracts.patient_id where status = $1 and worker_id = $2;',
       values: ["PENDING", userId]
     };
 
     db.query(query)
-      .then(result => res.json(result))
+      .then(result => {
+        res.json(result.map(({ firstname, lastname, ...item }) => ({
+          ...item,
+          patient: {
+            firstname,
+            lastname
+          }
+        })))
+      })
       .catch(err => {
         console.error(err);
         res.status(500).send(err)
