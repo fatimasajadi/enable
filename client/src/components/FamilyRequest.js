63b4f3301a6ffad79083eb2f838a92c76657423b
fsajadi <s.sfatemeh@gmail.com>
2020-08-10 13:46:24 -0400

    modify stylrs in myrequest page

--- client/src/components/FamilyRequest.js
+++ client/src/components/FamilyRequest.js
@@ -3,7 +3,7 @@ import './FamilyRequest.css'
 import { Button, ButtonGroup, ButtonDropdown, DropdownToggle, DropdownItem, DropdownMenu } from 'reactstrap';
 import ProfilePicture from '../images/profilePicture.PNG'
 import { Rating } from './Rating';
-import { Container, Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
+import { FormGroup, Row, Col } from 'reactstrap';
 
 function FamilyRequest(props) {
   const [rating, setRating] = useState(4);
@@ -11,37 +11,35 @@ function FamilyRequest(props) {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
+    <Col md={12}>
+      <Row className='pre-session-container'>
+        <Col md={2}>
+          <FormGroup>
+            <div className='img-name'>
+              <img src={ProfilePicture} alt='pending-req-pic' className='pre-session-pic'></img>
+              <p className='name'>{worker.firstname} {worker.lastname} </p>
+            </div>
+            <Rating value={rating} onChange={setRating} />
+          </FormGroup>
+        </Col>
 
-    <Row className='pre-session-container'>
-      <Col md={2}>
-        <FormGroup>
-          <div className='img-name'>
-            <img src={ProfilePicture} alt='pending-req-pic' className='pre-session-pic'></img>
-            <p className='name'>{worker.firstname} {worker.lastname} </p>
-          </div>
-          <Rating value={rating} onChange={setRating} />
-        </FormGroup>
-      </Col>
+        <Col md={4}>
+          <FormGroup className="description-rate-family">
+            <p><b>Description: </b> {props.value.description}</p>
+            <p><b>Rate: </b>${props.value.rate} /hour</p>
+          </FormGroup>
+        </Col>
+        <Col md={4}>
+          <FormGroup>
+            <div className='description-rate-p'>
+              <p><b>From: </b> {props.value.fromDate.format('ddd Do of MMM, LT')} </p>
+              <p><b>To: </b> {props.value.toDate.format('ddd Do of MMM, LT')}</p>
+            </div>
+          </FormGroup>
+        </Col>
 
-      <Col md={4}>
-        <FormGroup className="description-rate-family">
-          <p><b>Description: </b> {props.value.description}</p>
-          <p><b>Rate: </b>${props.value.rate} /hour</p>
-        </FormGroup>
-      </Col>
-      <Col md={4}>
-        <FormGroup>
-          <div className='description-rate-p'>
-            <p><b>From: </b> {props.value.fromDate.format('ddd Do of MMM, LT')} </p>
-            <p><b>To: </b> {props.value.toDate.format('ddd Do of MMM, LT')}</p>
-          </div>
-        </FormGroup>
-      </Col>
-
-      <div className='pre-session-button'>
-
-        {props.value.status === 'PENDING' && (
-          <Col md={2}>
+        <Col md={2} className="pt-4">
+          {props.value.status === 'PENDING' && (
             <FormGroup>
               <ButtonGroup>
                 <Button disabled color="secondary">{props.value.status}</Button>
@@ -53,20 +51,19 @@ function FamilyRequest(props) {
                 </ButtonDropdown>
               </ButtonGroup>
             </FormGroup>
-          </Col>
 
-        )
+          )
 
-        }
-        {props.value.status === 'ACCEPTED' &&
-          <Button disabled color="success">{props.value.status}</Button>
-        }
-        {props.value.status === 'REJECTED' &&
-          <Button disabled color="danger">{props.value.status}</Button>
-        }
-
-      </div>
-    </Row >
+          }
+          {props.value.status === 'ACCEPTED' &&
+            <Button disabled color="success">{props.value.status}</Button>
+          }
+          {props.value.status === 'REJECTED' &&
+            <Button disabled color="danger">{props.value.status}</Button>
+          }
+        </Col>
+      </Row>
+    </Col>
   );
 }
 
